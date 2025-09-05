ce123c53d219b1dc2abf1e9b674e7d84aaa4e1b5
wolskaskrzynka-art <wolska.skrzynka@gmail.com>
2025-09-05 09:51:09 +0200

    Update EventsSection.tsx

--- src/components/EventsSection.tsx
+++ src/components/EventsSection.tsx
@@ -149,10 +149,12 @@ Data zgłoszenia: ${new Date().toLocaleString("pl-PL")}
         {
           id: "13-3",
           time: "16:05",
-          speaker: "Anna Zielenkiewicz",
-          title: "PTSD i pierwsza pomoc emocjonalna.",
+          speaker: "Anna Zielenkiewicz / Sebastian Grzywacz",
+          title: "PTSD i pierwsza pomoc emocjonalna / Dotyk bez pytania – niewidzialne granice niewidomych",
           description:
-            "Co to jest zespół stresu pourazowego (PTSD), jakie są kryteria diagnostyczne. Czym charakteryzuje się pierwsza pomoc emocjonalna i jakie efekty przynosi. Jaka jest rola osób wspierających ludzi doświadczających przemocy.",
+            "Co to jest zespół stresu pourazowego (PTSD), jakie są kryteria diagnostyczne. Czym charakteryzuje się pierwsza pomoc emocjonalna i jakie efekty przynosi. Jaka jest rola osób wspierających ludzi doświadczających przemocy.
+
+Wystąpienie Sebastiana Grzywacza – przedsiębiorcy, trenera motywacyjnego i osoby niewidomej – porusza problem często niedostrzeganej znieczulicy wobec osób z niepełnosprawnością wzroku. W swoim wystąpieniu opowie o przekraczaniu granic fizycznych i emocjonalnych, z jakim mierzą się niewidomi na co dzień – od nagłego łapania za rękę, przez przesuwanie bez zgody, aż po ignorowanie potrzeby samodzielności. Wykład to zaproszenie do refleksji nad tym, jak okazywać pomoc z empatią i szacunkiem – nie „dla kogoś”, ale „z kimś”.",
         },
         {
           id: "13-4",
